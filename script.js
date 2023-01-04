41fb6c835546a9841bc359a4cd4d22ba2d27b472
rustamtolipov <rustamxontolipov788@gmail.com>
2023-01-05 02:17:41 +0500

    🚑

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
-const rulesBtn = document.querySelector(".btn-rules");
-const rulesModal = document.querySelector("#rules");
-const overlay = document.querySelector("#overlay");
-const closeRules = document.querySelector(".close--rules");
+const rulesBtn = document.querySelector('.btn-rules');
+const rulesModal = document.querySelector('#rules');
+const overlay = document.querySelector('#overlay');
+const closeRules = document.querySelector('.close--rules');
 
-rulesModal.style.display = "none";
-overlay.style.display = "none";
+rulesModal.style.display = 'none';
+overlay.style.display = 'none';
 
-rulesBtn.addEventListener("click", () => {
-  rulesModal.style.display = "flex";
-  overlay.style.display = "block";
+rulesBtn.addEventListener('click', () => {
+  rulesModal.style.display = 'flex';
+  overlay.style.display = 'block';
 });
 
-overlay.addEventListener("click", () => {
-  rulesModal.style.display = "none";
-  overlay.style.display = "none";
+overlay.addEventListener('click', () => {
+  rulesModal.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
-closeRules.addEventListener("click", () => {
-  rulesModal.style.display = "none";
-  overlay.style.display = "none";
+closeRules.addEventListener('click', () => {
+  rulesModal.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
 let player;
@@ -29,10 +29,10 @@ let komputer;
 
 let result;
 
-let options = ["rock", "paper", "scissors"];
+let options = ['rock', 'paper', 'scissors'];
 
-const kompTallaydi = () => {
-  komputer = Math.floor(Math.random() * 3);
+const kompTallaydi = (e) => {
+  komputer = Math.floor(Math.random() * e);
   return options[komputer];
 };
 
@@ -46,25 +46,39 @@ const durrang = () => {
 
 const kimYutdi = (player, komputer) => {
   if (durrang()) {
-    return "durrang";
+    return 'durrang';
   } else if (player === options[2] && komputer === options[1]) {
-    return "player";
+    return 'player';
   } else if (player === options[0] && komputer === options[2]) {
-    return "player";
+    return 'player';
   } else if (player === options[1] && komputer === options[0]) {
-    return "player";
+    return 'player';
   } else {
-    return "komputer";
+    return 'komputer';
   }
 };
 
 const playGame = (option) => {
   player = option;
-  komputer = kompTallaydi();
+  komputer = kompTallaydi(3);
   result = kimYutdi(player, komputer);
   console.log(player);
   console.log(komputer);
   console.log(result);
 };
 
-playGame("paper");
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+
+rockBtn.addEventListener('click', () => {
+  playGame('rock');
+});
+
+paperBtn.addEventListener('click', () => {
+  playGame('paper');
+});
+
+scissorsBtn.addEventListener('click', () => {
+  playGame('scissors');
+});
